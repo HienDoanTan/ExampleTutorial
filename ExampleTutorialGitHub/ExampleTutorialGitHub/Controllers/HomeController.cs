8b86499eccdaa98212f6f23450bb13633232f1ca
TanHien <tanhien.it@gmail.com>
2017-10-23 11:17:27 +0700

    thêm param

--- ExampleTutorialGitHub/ExampleTutorialGitHub/Controllers/HomeController.cs
+++ ExampleTutorialGitHub/ExampleTutorialGitHub/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace ExampleTutorialGitHub.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public ActionResult Test()
+        public ActionResult Test(string abc)
         {
             return View();
         }
